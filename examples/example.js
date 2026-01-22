5829c072c654b824b7d9b491a9eedf8571a16ce6
atifsayed22 <sayedatif4321@gmail.com>
2026-01-22 17:40:18 +0530

    chnba

--- examples/example.js
+++
@@ -1,7 +0,0 @@
-const { HackvisionClient } = require('../src/sdk');
-
-(async () => {
-  const client = new HackvisionClient({ apiKey: 'demo' });
-  const res = await client.echo('hello from SDK');
-  console.log(res);
-})();
